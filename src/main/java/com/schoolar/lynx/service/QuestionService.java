4563f1d134178919a2a1dfb9dcf3a1a34bd3ad1a
Juliano Almada <julianoalmada48@gmail.com>
2026-07-04 00:47:47 -0300

    feat: finalizado a função de create em QuestionService

--- src/main/java/com/schoolar/lynx/service/QuestionService.java
+++ src/main/java/com/schoolar/lynx/service/QuestionService.java
@@ -2,9 +2,12 @@ package com.schoolar.lynx.service;
 
 import com.schoolar.lynx.domain.dto.QuestionResponseDTO;
 import com.schoolar.lynx.domain.dto.RegisterQuestionDTO;
+import com.schoolar.lynx.domain.model.Company;
+import com.schoolar.lynx.domain.model.Question;
 import com.schoolar.lynx.domain.model.User;
 import com.schoolar.lynx.repository.QuestionRepository;
 import com.schoolar.lynx.security.AuthenticatedUserService;
+import com.schoolar.lynx.utils.MapperUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,7 +19,7 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
     private final AuthenticatedUserService authUserService;
 
-    public QuestionResponseDTO createQuestion(RegisterQuestionDTO dto){
+    public QuestionResponseDTO create(RegisterQuestionDTO dto){
         User loggedUser = authUserService.get();
         QuestionResponseDTO questionResponseDTO = new QuestionResponseDTO();
         if (!loggedUser.isAdmin()){
@@ -26,8 +29,8 @@ public class QuestionService {
             );
         }
         dto.setAuthorId(loggedUser.getId());
-        //questionRepository.save();
-        return questionResponseDTO;
-        //TODO: finalizar função
+        Question question = MapperUtil.parseObject(dto, Question.class);
+        return MapperUtil.parseObject(questionRepository.save(question), QuestionResponseDTO.class);
+        //TODO: testar
     }
 }
